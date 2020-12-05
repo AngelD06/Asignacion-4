a70dd83bae0b6ec8580027b5d360bc8f74dfb46f
AngelD06 <72891340+AngelD06@users.noreply.github.com>
2020-12-04 20:43:17 -0400

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using model;
+
+namespace asignacion_4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private InformacionGeneral Mipersona = new InformacionGeneral();
+
+        public ActionResult Index(int id =1)
+        {
+
+            return View(Mipersona.MostrarMiinformacion(id));
+        }
+    }
+}
